2974028fdec34faee7ae852f4b1f74dff1af123a
Wilfredo Villegas <wvillegasm@gmail.com>
2017-07-11 23:22:52 -0400

    Implementing Question Request

--- src/server/routes/question/question.js
+++ src/server/routes/question/question.js
@@ -4,6 +4,21 @@ import {BAD_REQUEST, CREATED, OK} from "http-status-codes";
 
 let question = express.Router();
 
+/*if (!QuestionModel.schema.options.toObject) QuestionModel.schema.options.toObject = {};
+QuestionModel.schema.options.toObject.transform = (doc, ret, options) => {
+  ret = {
+    id: ret.id,
+    content: {
+      title: ret.title,
+      text: ret.content.text,
+      categories: ret.content.categories
+    },
+    ranks: {
+      votes: ret.votes
+    }
+  };
+};*/
+
 question.post('/', (req, res) => {
   res.set('Content-Type', 'application/json');
 
@@ -26,12 +41,35 @@ question.post('/', (req, res) => {
 
 });
 
+question.get('/:id', (req, res) => {
+  res.set('Content-Type', 'application/json');
+  QuestionModel.findById(req.params.id, 'title votes content').
+  then(q => {
+
+    res.statusCode = OK;
+    res.json({
+      id: q.id,
+      content: {
+        title: q.title,
+        text: q.content.text,
+        categories: q.content.categories
+      },
+      ranks: {
+        votes: q.votes
+      }
+    });
+
+  }).catch(err => {
+    res.statusCode = BAD_REQUEST;
+    return res.json({err: err.message});
+  });
+});
 
 question.get('/', (req, res) => {
 
   console.log(req.query);
 
-  let {page = 0, limit = 0, offset = 0, sort = 'createdOn'} = req.query;
+  let {page = 0, limit = 0, offset = 0, sort = '-createdOn'} = req.query;
 
   if (page !== 0) page = Number.parseInt(page, 10);
   if (limit !== 0) limit = Number.parseInt(limit, 10);
@@ -40,7 +78,7 @@ question.get('/', (req, res) => {
   res.set('Content-Type', 'application/json');
   QuestionModel.paginate(
     {},
-    {select: 'id votes content createdOn', page: page, limit: limit, offset: offset, sort: sort}
+    {select: 'id title votes content createdOn', page: page, limit: limit, offset: offset, sort: sort}
   )
     .then(qs => {
 
@@ -49,15 +87,38 @@ question.get('/', (req, res) => {
       console.log('total: ', qs.total);
 
       res.statusCode = OK;
-      res.json(qs);
+
+      let tList = qs.docs.map((r)=>{
+        return {
+          id: r.id,
+          content: {
+            title: r.title,
+            text: r.content.text,
+            categories: r.content.categories
+          },
+          ranks: {
+            votes: r.votes
+          }
+        };
+      });
+
+      res.json({
+        "docs": tList,
+        "total": qs.total,
+        "limit": qs.limit,
+        "offset": qs.offset
+
+      });
 
 
     })
     .catch(err => {
       res.statusCode = BAD_REQUEST;
       res.json({err: err.message});
-    });
+    })
 
 });
+
+
 
 export default question;
